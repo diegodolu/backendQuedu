498accdb1dd5a37918e5194eaf774ef37f80d755
Diego Bejarano <diego.bejarano@tecsup.edu.pe>
2024-10-13 16:24:21 -0500

    crear curso added

--- routes/user.js
+++ routes/user.js
@@ -6,5 +6,6 @@ const router = express.Router();
 router.get('/users', UserController.getUsers);
 router.get('/user/:id', UserController.getUserById);
 router.post('/user', UserController.createUser);
+router.post('/user/course/new', UserController.createCourse);
 
 module.exports = router;
