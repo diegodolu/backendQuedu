f30c93528f8a409f1a07a98ab367b209f0965b08
Diego Bejarano <diego.bejarano@tecsup.edu.pe>
2024-11-11 11:31:16 -0500

    community controller modified

--- routes/community.js
+++ routes/community.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const CommunityController = require('../controllers/CommunityController');
+const verificarToken = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
-router.get('/communities', CommunityController.getCommunity);
-router.post('/community/new', CommunityController.createCommunity);
+router.get('/communities', verificarToken, CommunityController.getCommunity);
+router.post('/community/new', verificarToken, CommunityController.createCommunity);
+router.post('/community', verificarToken, CommunityController.getCommunityById);
 
 module.exports = router;
