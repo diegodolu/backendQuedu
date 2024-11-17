e9524cb642b9f2a1e8c555588d81a4b096257e29
santiagopiscontetecsup <santiago.pisconte@tecsup.edu.pe>
2024-11-16 19:53:03 -0500

    lista de quedus

--- routes/user.js
+++ routes/user.js
@@ -15,6 +15,9 @@ router.post('/user/course/new', verificarToken, UserController.createCourse);
 router.post('/user/course/quedu/new', UserController.createPersonalQuedus);
 router.post('/user/lastQuedu', verificarToken, UserController.getLastQuedu);
 router.get('/user/quedus/:id', verificarToken, UserController.getRecentPersonalQuedusByUser);
+
+router.get('/user/course/quedus', verificarToken, UserController.getQuedusByCourseId);
+
 router.put('/quedu/update', verificarToken, UserController.updateQuedu);
 router.post('/user/subscribeTo', verificarToken, UserController.subscribeToCommunity);
 router.post('/user/shareQuedu', verificarToken, UserController.sharePersonalQuedu);
